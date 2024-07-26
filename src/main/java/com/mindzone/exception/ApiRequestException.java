dda13a66824b15ac7a63ac40c77bf3644b0a393e
Vinícius Azevedo <sousa0240@gmail.com>
2024-07-26 17:24:00 -0300

    creates custom ApiRequestException

---
+++ src/main/java/com/mindzone/exception/ApiRequestException.java
@@ -0,0 +1,12 @@
+package com.mindzone.exception;
+
+public class ApiRequestException extends RuntimeException {
+
+    public ApiRequestException(String message) {
+        super(message);
+    }
+
+    public ApiRequestException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
