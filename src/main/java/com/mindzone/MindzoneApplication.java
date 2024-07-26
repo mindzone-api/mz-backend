480125f46aea64131a2867d0dc57198f0fa0aac9
Vinícius Azevedo <sousa0240@gmail.com>
2024-07-26 17:06:47 -0300

    initial commit

---
+++ src/main/java/com/mindzone/MindzoneApplication.java
@@ -0,0 +1,13 @@
+package com.mindzone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindzoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindzoneApplication.class, args);
+	}
+
+}
