34a4f155385acd0c0ee520b65196b35fac8cfde5
Vinícius Azevedo <sousa0240@gmail.com>
2024-07-26 22:05:24 -0300

    adds slf4j

--- src/main/java/com/mindzone/controller/UserController.java
+++ src/main/java/com/mindzone/controller/UserController.java
@@ -3,6 +3,9 @@ package com.mindzone.controller;
 import com.mindzone.model.User;
 import com.mindzone.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +14,7 @@ import java.util.List;
 
 @RestController
 @AllArgsConstructor
+@Slf4j
 public class UserController {
 
     private UserRepository userRepository;
