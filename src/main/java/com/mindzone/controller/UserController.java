02f287446e1bb4d25c8d973e7f610d47b1146c92
Vinícius Azevedo <sousa0240@gmail.com>
2024-08-01 17:25:47 -0300

    Update UserController.java

--- src/main/java/com/mindzone/controller/UserController.java
+++ src/main/java/com/mindzone/controller/UserController.java
@@ -1,23 +1,13 @@
 package com.mindzone.controller;
 
-import com.mindzone.model.User;
-import com.mindzone.repository.UserRepository;
-import com.mindzone.service.MailServiceImpl;
-import com.mindzone.service.impl.UserService;
+
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.security.Principal;
-import java.util.Collections;
-import java.util.Map;
 
 import static com.mindzone.util.Constants.V1;
 
@@ -27,9 +17,9 @@ import static com.mindzone.util.Constants.V1;
 @RequestMapping(V1 + "/user")
 public class UserController {
 
-    @GetMapping
-    public String getUserInfo(JwtAuthenticationToken principal) {
-        return (String) principal.getTokenAttributes().get("email");
+    @GetMapping("/email")
+    public String getUserEmail(JwtAuthenticationToken jwtAuth) {
+        return (String) jwtAuth.getTokenAttributes().get("email");
     }
 
     @GetMapping("/test")
