b223106cd94dcc1f526f4b1a8893deffbbfe5058
Vinícius Azevedo <sousa0240@gmail.com>
2024-08-01 20:51:50 -0300

    updates constants

--- src/main/java/com/mindzone/exception/ExceptionMessages.java
+++ src/main/java/com/mindzone/exception/ExceptionMessages.java
@@ -1,4 +1,15 @@
 package com.mindzone.exception;
 
 public class ExceptionMessages {
+
+    // --------------- USER --------------- \\
+
+    public static final String USER_NOT_FOUND = "USER NOT FOUND";
+    public static final String USER_UNAUTHORIZED = "USER UNAUTHORIZED";
+    public static final String USER_ALREADY_EXISTS = "USER ALREADY EXISTS";
+
+    // --------------- SESSIONS --------------- \\
+
+    public static final String INVALID_TIME_RANGE = "INVALID TIME RANGE";
+
 }
