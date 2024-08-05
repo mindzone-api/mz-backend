5cec0a807cb0eb1e126c1ea549fd56e1ecadcee1
Vinícius Azevedo <sousa0240@gmail.com>
2024-08-04 21:09:19 -0300

    switch professional controller to patient

--- src/main/java/com/mindzone/controller/PatientController.java
+++ src/main/java/com/mindzone/controller/PatientController.java
@@ -4,27 +4,25 @@ import com.mindzone.dto.request.SearchFilter;
 import com.mindzone.model.user.User;
 import com.mindzone.service.impl.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.data.domain.Page;
 
 import static com.mindzone.constants.Constants.V1;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping(V1 + "/professional")
-public class ProfessionalController {
+@RequestMapping(V1 + "/patient")
+public class PatientController {
 
     private UserService userService;
-
     @GetMapping("/search")
     public ResponseEntity<Page<User>> search(JwtAuthenticationToken token, @RequestBody SearchFilter filter) {
         userService.validateUser(token);
         return ResponseEntity.ok(userService.search(filter));
     }
-
 }
