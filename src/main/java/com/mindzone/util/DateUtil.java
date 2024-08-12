3d29bbd49b4a25f54d23b9940f622dd74430995e
Vinícius Azevedo <sousa0240@gmail.com>
2024-08-12 18:01:14 -0300

    implements javadoc on DateUtil

--- src/main/java/com/mindzone/util/DateUtil.java
+++ src/main/java/com/mindzone/util/DateUtil.java
@@ -5,6 +5,13 @@ import java.time.ZoneId;
 import java.util.Date;
 
 public class DateUtil {
+
+    /**
+     * Calculates the minimum birth date (from that date until today)
+     * possible from the age informed
+     * @param age informed to calculate the minimum birth date
+     * @return the minimum birth date
+     */
     public static Date calculateBirthFromMinimumAge(int age) {
         LocalDate now = LocalDate.now();
         LocalDate birthDateLimit = now.minusYears(age);
@@ -13,6 +20,12 @@ public class DateUtil {
         return Date.from(birthDateLimit.atStartOfDay(ZoneId.systemDefault()).toInstant());
     }
 
+    /**
+     * Calculates the maximum birth date (from that date backwards)
+     * possible from the age informed
+     * @param age informed to calculate the maximum birth date
+     * @return the maximum birth date
+     */
     public static Date calculateBirthFromMaximumAge(int age) {
         LocalDate now = LocalDate.now();
         LocalDate birthDateLimit = now.minusYears(age);
