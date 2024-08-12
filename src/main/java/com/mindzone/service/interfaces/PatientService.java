e30ae5ce8652d39aa970b7725b953c20dfdf81f6
Vinícius Azevedo <sousa0240@gmail.com>
2024-08-12 18:01:06 -0300

    removes therapy request from patientService

--- src/main/java/com/mindzone/service/interfaces/PatientService.java
+++ src/main/java/com/mindzone/service/interfaces/PatientService.java
@@ -9,6 +9,4 @@ import java.util.List;
 
 public interface PatientService {
     List<ListedProfessional> getMyProfessionals(User user);
-
-    TherapyResponse requestTherapy(TherapyRequest therapyRequest, String userId);
 }
