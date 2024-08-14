c5be5497ba2a417aecd99ae1f0f2c82d09a42dd5
Vinícius Azevedo <sousa0240@gmail.com>
2024-08-14 16:19:47 -0300

    [WIP] implements feature skeleton

--- src/main/java/com/mindzone/service/impl/TherapyServiceImpl.java
+++ src/main/java/com/mindzone/service/impl/TherapyServiceImpl.java
@@ -1,10 +1,9 @@
 package com.mindzone.service.impl;
 
 import com.mindzone.dto.request.TherapyRequest;
+import com.mindzone.dto.request.TherapyRequestAnalysis;
 import com.mindzone.dto.response.TherapyResponse;
 import com.mindzone.dto.response.listed.ListedTherapy;
-import com.mindzone.enums.Role;
-import com.mindzone.enums.TherapyStatus;
 import com.mindzone.exception.ApiRequestException;
 import com.mindzone.model.therapy.Therapy;
 import com.mindzone.model.user.ProfessionalInfo;
@@ -23,7 +22,10 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import static com.mindzone.constants.MailsBody.therapyRequestMail;
+import static com.mindzone.constants.MailsBody.*;
+import static com.mindzone.enums.Role.PATIENT;
+import static com.mindzone.enums.Role.PROFESSIONAL;
+import static com.mindzone.enums.TherapyStatus.*;
 import static com.mindzone.exception.ExceptionMessages.*;
 import static com.mindzone.util.WeekDayScheduleUtil.fitsIn;
 import static com.mindzone.util.WeekDayScheduleUtil.overlaps;
@@ -38,20 +40,21 @@ public class TherapyServiceImpl implements TherapyService {
     private UltimateModelMapper m;
 
     @Override
-    public TherapyResponse requestTherapy(TherapyRequest therapyRequest, User user) {
-        checkTherapyRequestValidation(therapyRequest, user);
+    public TherapyResponse requestTherapy(TherapyRequest therapyRequest, User patient) {
+        checkTherapyRequestValidation(therapyRequest, patient);
 
         // initialize and save request
         Therapy therapy = m.map(therapyRequest, Therapy.class);
-        therapy.setPatientId(user.getId());
-        therapy.setTherapyStatus(TherapyStatus.PENDING);
+        therapy.setPatientId(patient.getId());
+        therapy.setTherapyStatus(PENDING);
         therapy.setCompletedSessions(new ArrayList<>());
+        therapy.setCreatedAt(new Date());
         save(therapy);
 
         // notify requested professional
         mailService.sendMail(therapyRequestMail(
                 userService.getById(therapyRequest.getProfessionalId()).getEmail(),
-                user.getName()
+                patient.getName()
         ));
 
         return m.map(therapy, TherapyResponse.class);
@@ -60,29 +63,69 @@ public class TherapyServiceImpl implements TherapyService {
     @Override
     public TherapyResponse get(User user, String id) {
         Therapy therapy = getById(id);
-        validateTherapyaccess(user, therapy);
+        canAccess(user, therapy);
         return m.map(therapy, TherapyResponse.class);
     }
 
     @Override
     public List<ListedTherapy> getAll(User user) {
         return (
-                user.getRole() == Role.PATIENT ?
+                user.getRole() == PATIENT ?
                         m.mapToList(therapyRepository.findAllByPatientId(user.getId()), ListedTherapy.class) :
                         m.mapToList(therapyRepository.findAllByProfessionalId(user.getId()), ListedTherapy.class)
         );
     }
 
     @Override
-    public TherapyResponse updateRequest(User user, String therapyId, TherapyRequest therapyRequest) {
-        Therapy therapy = getById(therapyId);
-        validateTherapyaccess(user, therapy);
-        if (therapy.getTherapyStatus() != TherapyStatus.PENDING) {
+    public TherapyResponse updateRequest(User patient, String id, TherapyRequest therapyRequest) {
+        Therapy therapy = getById(id);
+        canManage(patient, therapy);
+        if (therapy.getTherapyStatus() != PENDING) {
             throw new ApiRequestException(THERAPY_IS_NOT_EDITABLE_ANYMORE);
         }
-        checkTherapyRequestValidation(therapyRequest, user);
+        checkTherapyRequestValidation(therapyRequest, patient);
         m.map(therapyRequest, therapy);
         save(therapy);
+        return m.map(therapy, TherapyResponse.class);
+    }
+
+    @Override
+    public TherapyResponse analyseRequest(User professional, String id, TherapyRequestAnalysis analysis) {
+        Therapy therapy = getById(id);
+        // Checking if only allowed answers were sent
+        if (analysis.getStatus() == PENDING || therapy.getTherapyStatus() == DENIED) {
+            throw new ApiRequestException(INVALID_THERAPY_REQUEST_ANALYSIS);
+        }
+
+        // Checking if the denial was sent with a justification text
+        if (
+                analysis.getStatus() == DENIED &&
+                (analysis.getDenialJustification() == null || analysis.getDenialJustification().isBlank())
+        ) {
+            throw new ApiRequestException(MISSING_DENIAL_JUSTIFICATION);
+        }
+
+
+        User patient = userService.getById(therapy.getPatientId());
+        canManage(professional, therapy);
+
+        // TODO 1- negar automaticamente todos os choques de horário em status pendente para o profissional
+        // TODO 2- negar automaticamente todas as solicitações feitas do paciente para outros profissionais do mesmo ramo
+        // TODO 3- negar automaticamente  todos os choques de horário em status pendente criados pelo usuário
+        // TODO 4- atualizar horários disponíveis do profissional
+        // TODO 5- US15
+
+        // update and save
+        therapy.setTherapyStatus(analysis.getStatus());
+        save(therapy);
+
+        // notify patient about professional final analysis
+        mailService.sendMail(
+                analysis.getStatus() == APPROVED ?
+                        approvedTherapyRequestMail(patient.getEmail(), professional.getName()) :
+                        deniedTherapyRequestMail(patient.getEmail(), professional.getName(), analysis.getDenialJustification())
+        );
+
         return m.map(therapy, TherapyResponse.class);
     }
 
@@ -105,6 +148,17 @@ public class TherapyServiceImpl implements TherapyService {
             throw new ApiRequestException(THERAPY_MODALITY_NOT_ACCEPTED);
         }
 
+        // FIXME - TESTAR VALORES NULOS ENTRE METODO DE PAGAMENTO E PLANO DE SAÚDE
+        // Checking if the payment method is accepted by the professional
+        if (!requestedProfessionalData.getPaymentMethods().contains(therapyRequest.getPaymentMethod())) {
+            throw new ApiRequestException(PAYMENT_METHOD_NOT_ACCEPTED);
+        }
+
+        // Checking if the health plan is accepted by the professional
+        if (!requestedProfessionalData.getAcceptedHealthPlans().contains(therapyRequest.getHealthPlan())) {
+            throw new ApiRequestException(HEALTH_PLAN_NOT_ACCEPTED);
+        }
+
         // Checking if the amount of time requested fits in the requested professional SessionDuration
         long professionalSessionDuration = requestedProfessionalData.getSessionDuration().toMinutes();
         for (WeekDaySchedule weekDaySchedule : therapyRequest.getSchedule()) {
@@ -116,7 +170,7 @@ public class TherapyServiceImpl implements TherapyService {
             }
         }
 
-        List<Therapy> patientTherapies = therapyRepository.findAllByPatientIdAndTherapyStatus(user.getId(), TherapyStatus.APPROVED);
+        List<Therapy> patientTherapies = therapyRepository.findAllByPatientIdAndTherapyStatus(user.getId(), APPROVED);
         for (Therapy patientTherapy : patientTherapies) {
             // Checking if patient is already in therapy with the profession requested
             User activeProfessional = userService.getById(patientTherapy.getProfessionalId());
@@ -137,17 +191,25 @@ public class TherapyServiceImpl implements TherapyService {
         }
     }
 
-    private void validateTherapyaccess(User user, Therapy therapy) {
-        if (user.getRole() == Role.PATIENT) {
+    private void canAccess(User user, Therapy therapy) {
+        if (user.getRole() == PATIENT) {
             if (!therapy.getPatientId().equals(user.getId())) {
                 throw new ApiRequestException(USER_UNAUTHORIZED);
             }
-        } else if (user.getRole() == Role.PROFESSIONAL) {
-            List<Therapy> patientTherapies = therapyRepository.findAllByPatientIdAndTherapyStatus(therapy.getPatientId(), TherapyStatus.APPROVED);
+        } else if (user.getRole() == PROFESSIONAL) {
+            List<Therapy> patientTherapies = therapyRepository.findAllByPatientIdAndTherapyStatus(therapy.getPatientId(), APPROVED);
             List<Therapy> patientTherapiesWithLoggedProfessional = patientTherapies.stream().filter(t -> t.getProfessionalId().equals(user.getId())).toList();
             if (patientTherapiesWithLoggedProfessional.isEmpty()) {
                 throw new ApiRequestException(USER_UNAUTHORIZED);
             }
+        }
+    }
+
+    private void canManage(User user, Therapy therapy) {
+        if (user.getRole() == PATIENT && !therapy.getPatientId().equals(user.getId())) {
+            throw new ApiRequestException(USER_UNAUTHORIZED);
+        } else if (user.getRole() == PROFESSIONAL && !therapy.getProfessionalId().equals(user.getId())) {
+            throw new ApiRequestException(USER_UNAUTHORIZED);
         }
     }
 }
