8a1d956e3a8f7f68d2049a19b11073f69f0f4f6a
Vinícius Azevedo <sousa0240@gmail.com>
2024-09-26 12:08:20 -0300

    fix user to listed professional

--- src/main/java/com/mindzone/util/UltimateModelMapper.java
+++ src/main/java/com/mindzone/util/UltimateModelMapper.java
@@ -7,6 +7,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
